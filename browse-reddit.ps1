c7324adf97192a9fdf77e16a62f3e3fbe818130e
Stephen Owen <sred13@gmail.com>
2011-12-07 11:44:20 -0500

    adding Pscript

---
+++ browse-reddit.ps1
@@ -0,0 +1,11 @@
+﻿# Browse-Reddit Script
+
+#Invoke-RestMethod http://www.reddit.com/.rss | Select Title, Link
+$i=1
+$frontpage=Invoke-RestMethod http://www.reddit.com/.rss
+$frontpage | Select-Object @{name = 'Article Number'; expression = {$i;$global:i++;}}, @{name = 'Name'; expression = {$_.Title;}}
+
+$article = Read-host "Select Article to read:"
+$article=$article-1
+$frontpage[$article].Title
+
